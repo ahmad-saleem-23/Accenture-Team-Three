98d5952e00e72f0c9e212b2e723a11e306f42a19
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-06-28 07:16:37 +1200

    created and seeded db

--- back-end/src/database/migrations/20230627184327_paymentDates.ts
+++ back-end/src/database/migrations/20230627184327_paymentDates.ts
@@ -2,9 +2,20 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('paymentDates', (table) => {
+    table.increments('id')
+    table.integer('subscriptionId')
+    table.date('scheduleDate')
+    table.timestamps(true, true)
+    table.boolean('isLastDate')
+    table.boolean('isPaid')
+ 
+  })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+  knex.schema.dropTable('paymentDates')
 }
+
 
