7abac7937e1c5445d8bafc220abfb54565acd0a0
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-06-28 08:12:32 +1200

    created db functions

---
+++ back-end/src/database/db.ts
@@ -0,0 +1,56 @@
+import connection from './connection'
+
+export function getAllSubscriptions(db = connection) {
+  return db('subscriptions').select()
+}
+
+export function getSubscriptionById(id: number, db = connection) {
+  return db('subscriptions')
+    .where('id', id)
+    .first()
+}
+
+
+type newSubscription = { name: string, frequency: string, startDate: string, endDate: string, category: string, website?: string, price: number, reminder?: boolean }
+
+export function addSubscription(subscription: newSubscription, db = connection) {
+  return db('subscriptions')
+    .insert(subscription)
+}
+
+type updatedSubscription = { name?: string, frequency?: string, startDate?: string, endDate?: string, category?: string, website?: string, price?: number, reminder?: boolean }
+
+export function updateSubscription(id: number, subscription: updatedSubscription, db = connection) {
+  return db('subscriptions')
+    .where('id', id)
+    .update(subscription)
+}
+
+export function deleteSubscription(id: number, db = connection) {
+  return db('subscriptions')
+    .where('id', id)
+    .delete()
+}
+
+export function getAllPaymentDates(db = connection) {
+  return db('paymentDates')
+    .join('subscriptions', 'paymentDates.subscriptionId', 'subscriptions.id')
+    .select('paymentDates.id', 'paymentDates.subscriptionId', 'paymentDates.date', 'subscriptions.name', 'subscriptions.frequency', 'subscriptions.startDate', 'subscriptions.endDate', 'subscriptions.category', 'subscriptions.website', 'subscriptions.price', 'subscriptions.reminder')
+
+}
+
+export function deletePaymentDates(subscriptionId: number, db = connection) {
+  return db('paymentDates')
+    .where('subscriptionId', subscriptionId)
+    .delete()
+}
+
+interface PaymentsDatesProp {
+  scheduleDate?: string
+  isLastDate?: boolean
+}
+
+export function addPaymentDates( subscriptionId: number, paymentsDates: PaymentsDatesProp, db = connection) {
+  return db('paymentDates')
+    .insert({ subscriptionId, ...paymentsDates })
+}
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getAllSubscriptions,addSubscription, deleteSubscription,updateSubscription,getAllPaymentDates,deletePaymentDates, getSubscriptionById } from './database/db';
 
 const server = express();
 
@@ -10,8 +11,37 @@ server.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-server.get('/profile', (req, res) => {
-  res.send('Hello, world!');
+server.get('/add', async (req, res) => {
+  
+  const subscriptions = await getAllSubscriptions();
+  res.json(subscriptions);
+});
+
+server.post('/add', async (req, res) => {
+  const subscription = req.body;
+  await addSubscription(subscription);
+  res.json(subscription);
+  
+
+});
+
+server.delete('/delete/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  await deleteSubscription(id);
+  res.json(id);
+});
+
+server.patch('/update/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const subscription = req.body;
+  await updateSubscription(id, subscription);
+  res.json(subscription);
+});
+
+server.get('/subscriptions/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const subscription = await getSubscriptionById(id);
+  res.json(subscription);
 });
 
 export default server;
